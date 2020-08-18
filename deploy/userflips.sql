0a9fe8a80a92a065a802c66292bf3d15dc32eba8
sandeepr29 <55761018+sandeepr29@users.noreply.github.com>
2020-08-18 11:36:13 +0530

    Update userflips.sql

--- deploy/userflips.sql
+++ deploy/userflips.sql
@@ -3,8 +3,10 @@
 -- requires: users
 -- requires: flips
 
+/**
 USE WAREHOUSE &warehouse;
 CREATE OR REPLACE VIEW flipr.userflips AS
 SELECT f.id, u.nickname, u.fullname, u.twitter, f.body, f.timestamp
   FROM flipr.users u
   JOIN flipr.flips f ON u.nickname = f.nickname;
+**/
